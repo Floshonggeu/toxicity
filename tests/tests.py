df064813545dcc96d885ad6f161800671e32056f
RemiGUIGUET <93648735+RemiGUIGUET@users.noreply.github.com>
2022-03-07 17:25:42 +0100

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -3,12 +3,9 @@ import warnings
 
 from requests import api
 warnings.filterwarnings("ignore")
-
-# from api import app
-
 import unittest
 import requests
-
+import time 
 class TestApi(unittest.TestCase):
 
     URL = "http://host.docker.internal:4000"
@@ -17,5 +14,21 @@ class TestApi(unittest.TestCase):
         r = requests.get(TestApi.URL)
         self.assertTrue(r.status_code == 200)
 
+
+    def test_stress(self):
+
+        self.startTime= time.time()
+        for i in range (100):
+            doc=requests.get(TestApi.URL)
+        t=time.time()-self.startTime
+        self.assertTrue(t<60)
+
+#    def test_output(self):
+#        r=[]
+#        r1=requests.get(TestApi.URL)
+#        r2=requests.get(TestApi.URL)
+#        r.append(r1)
+#        r.append(r2)
+#        self.assertEqual(r,["toxic","neutral"])
 if __name__ == '__main__':
     unittest.main()
